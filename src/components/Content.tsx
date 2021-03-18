1ef0843bf9c34647b53a09ea756d92ea8e4e875e
Gabriel Paolini <paolini396@gmail.com>
2021-03-18 08:42:48 -0300

    feat: components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,58 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+
+import { api } from '../services/api';
+
+import { MovieCard } from './MovieCard';
+import { Header } from './Header';
+
+import '../styles/content.scss';
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  selectedGenreId: number;
+}
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.selectedGenreId}`)
+      .then(response => {
+        setMovies(response.data);
+      });
+  },[props.selectedGenreId])
+
+  return (
+    <div className="container">
+      <Header title={props.selectedGenre.title} />
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard 
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value} 
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+import '../styles/header.scss';
+
+interface HeaderProps {
+  title: string;
+}
+
+export function Header(props: HeaderProps) {
+  return (
+    <header>
+      <span className="category">Categoria:<span> {props.title}</span></span>
+    </header>
+  );
+}
